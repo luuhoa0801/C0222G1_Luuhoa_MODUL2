c4fc624c9e40c55479d7a909ef2076595cb6d6e0
luuhoa0801 <hoathuytien12369@gmail.com>
2022-04-22 10:15:05 +0700

    up

--- c02g1_java/src/_16_io_text_file/exercise/read_file_csv/Main.java
+++ c02g1_java/src/_16_io_text_file/exercise/read_file_csv/Main.java
@@ -8,8 +8,9 @@ public class Main {
     public static void main(String[] args) {
         List<Nation> arrayList = new ArrayList<>();
         List<String[]> stringsList =  ReadAndWrite.readFile("src\\_16_io_text_file\\exercise\\read_file_csv\\test.csv");
+        Nation nation;
         for (String[] item: stringsList) {
-            Nation nation = new Nation(Integer.parseInt(item[0]),item[1],item[2]);
+             nation = new Nation(Integer.parseInt(item[0]),item[1],item[2]);
             arrayList.add(nation);
         }
         for (Nation item: arrayList) {
